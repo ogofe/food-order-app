483217b741c708b1968d948df9df8a24b11d6b13
Ogofe <7thogofe@gmail.com>
2022-03-28 02:29:44 +0100

    Update settings.py

--- hotspot/settings.py
+++ hotspot/settings.py
@@ -4,7 +4,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.getenv('DJANGO_SECRET')
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [ "jtogofe-hotspot.herokuapp.com", ]
 
